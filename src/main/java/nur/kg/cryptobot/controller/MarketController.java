b56aa37146dfe2756bf271df5201b996215ccd41
nurdas <nurdas00@gmail.com>
2025-09-27 23:36:35 +0600

    add reactor and common-lib

--- src/main/java/nur/kg/cryptobot/controller/MarketController.java
+++ src/main/java/nur/kg/cryptobot/controller/MarketController.java
@@ -1,11 +1,12 @@
 package nur.kg.cryptobot.controller;
 
 import lombok.RequiredArgsConstructor;
-import nur.kg.cryptobot.dto.ProcessMarketRequestDto;
 import nur.kg.cryptobot.service.MarketService;
-import org.springframework.http.ResponseEntity;
+import nur.kg.domain.dto.TickerDto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,10 +15,9 @@ public class MarketController {
     private final MarketService marketService;
 
     @PostMapping("/process")
-    public ResponseEntity<?> processMarket(ProcessMarketRequestDto requestDto) {
+    public Mono<Void> processMarket(Flux<TickerDto> requestDto) {
 
         marketService.processMarket(requestDto);
 
-        return ResponseEntity.ok().build();
     }
 }
